7ce3c9e94a9282b31860f45eb0f6f6fc896da42d
Alice D'Costa <103961537+AliceBendinha@users.noreply.github.com>
2022-05-25 12:07:55 +0100

    tabela de produtos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import "./App.css";
 
-function MyButton() {
+
+function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
   return (
-    <button>
-      Click me
-    </button>
+    <div className='Tabela'>
+      <SearchBar 
+        filterText={filterText} 
+        inStockOnly={inStockOnly} 
+        onFilterTextChange={setFilterText} 
+        onInStockOnlyChange={setInStockOnly} />
+      <ProductTable 
+        products={products} 
+        filterText={filterText}
+        inStockOnly={inStockOnly} />
+    </div>
   );
 }
 
-export default function MyApp() {
+function ProductCategoryRow({ category }) {
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
+    <tr>
+      <th colSpan="2">
+        {category}
+      </th>
+    </tr>
   );
+}
+
+function ProductRow({ product }) {
+  const name = product.stocked ? product.name :
+    <span style={{ color: 'rgb(253, 4, 4)' }}>
+      {product.name}
+    </span>;
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+
+
+function ProductTable({ products, filterText, inStockOnly }) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (
+      product.name.toLowerCase().indexOf(
+        filterText.toLowerCase()
+      ) === -1
+    ) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category} />
+      );
+    }
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.name} />
+    );
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Produto</th>
+          <th>Preço</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function SearchBar({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange
+}) {
+  return (
+    <form>
+      <input 
+        type="text" 
+        value={filterText} placeholder="Pesquisar..." 
+        onChange={(e) => onFilterTextChange(e.target.value)} />
+      <label>
+        <input 
+          type="checkbox" 
+          checked={inStockOnly} 
+          onChange={(e) => onInStockOnlyChange(e.target.checked)} />
+        {' '}
+        Apenas produtos disponíveis
+      </label>
+    </form>
+  );
+}
+
+const PRODUCTS = [
+  {category: "Doces e Salgados", price: "50,00kz", stocked: true, name: "Bola de berlim"},
+  {category: "Doces e Salgados", price: "50,00kz", stocked: false, name: "ckeque"},
+  {category: "Doces e Salgados", price: "200,00kz", stocked: true, name: "Bolo de Chocolate (Fatia)"},
+  {category: "Doces e Salgados", price: "100,00kz", stocked: false, name: "Ressol"}, 
+  {category: "Doces e Salgados", price: "75,00kz", stocked: true, name: "Pastel"},
+  {category: "Sumos Naturais", price: "150,00kz", stocked: true, name: "Sumo de gajaja"},
+  {category: "Sumos Naturais", price: "200,00kz", stocked: true, name: "Sumo de múcua com beterraba"},
+  {category: "Sumos Naturais", price: "350,00kz", stocked: false, name: "Sumo rosa"},
+  {category: "Sumos Naturais", price: "500,00kz", stocked: true, name: "Sumo tuti (manga, laranja, cenoura e ortelã)"},
+];
+
+export default function App() {
+  return <FilterableProductTable products={PRODUCTS} />;
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { keyboardImplementationWrapper } from '@testing-library/user-event/dist/keyboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
